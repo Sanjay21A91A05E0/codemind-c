424bed7788fe36972ba0013e5d64b7eae1545f69
Sanjay kumar <97424311+Sanjay21A91A05E0@users.noreply.github.com>
2022-04-19 17:55:26 +0530

    #include<stdio.h> int main() {     int n,i,s=0,a[100],c=0;     float avg;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         s+=a[i];     }     avg=s/n;     for(i=0;i<n;i++)     {         if(a[i]<=avg)         c++;     }     printf("%d",c); }

---
+++ elements_smaller_than_average.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,s=0,a[100],c=0;
+    float avg;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        s+=a[i];
+    }
+    avg=s/n;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<=avg)
+        c++;
+    }
+    printf("%d",c);
+}
