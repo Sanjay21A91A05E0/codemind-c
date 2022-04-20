c969ec11fb3502230cdadd600b86001eb1bf3db8
Sanjay kumar <97424311+Sanjay21A91A05E0@users.noreply.github.com>
2022-04-20 09:55:35 +0530

    #include<stdio.h> int main() {     int i,j,n,a[200],c=0,f=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(i!=j)             {                 if(a[i]==a[j])                 c++;             }         }        if(c==0)       {          if(a[i]%2)          {              f++;          }       }     }    printf("%d",f); }

---
+++ odd_unique_elements_count.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,a[200],c=0,f=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(i!=j)
+            {
+                if(a[i]==a[j])
+                c++;
+            }
+        }
+       if(c==0)
+      {
+         if(a[i]%2)
+         {
+             f++;
+         }
+      }
+    }
+   printf("%d",f);
+}
